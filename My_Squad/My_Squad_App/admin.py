5d641cb629204783a25aea20bfbe489fde419885
684marcel <mmichalczyk1@st.swps.edu.pl>
2025-01-06 14:20:24 +0100

    coś tu naszponiłem nie wiem co dokładnie

--- My_Squad/My_Squad_App/admin.py
+++ My_Squad/My_Squad_App/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Team, Osoba, Stanowisko
+
+admin.site.register(Team)
+admin.site.register(Osoba)
+admin.site.register(Stanowisko)
+
+class OsobaAdmin(admin.ModelAdmin):
+    @admin.display(description=" Stanowisko (ID)")
+    def stanowisko_with_id(self, obj):
+        if obj.stanowisko :
+            return f'{obj.stanowisko} ({obj.stanowisko})'
+        return 'Brak stanowiska'
+    list_display = ["imie", "nazwisko", "plec", "stanowisko_with_id", "data_dodania"]
+
+admin.site.register(Osoba, OsobaAdmin)
--- My_Squad/My_Squad_App/models.py
+++ My_Squad/My_Squad_App/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from datetime import date
 
-# Create your models here.
+MONTHS = models.IntegerChoices('Miesiace', 'Styczeń Luty Marzec Kwiecień Maj Czerwiec Lipiec Sierpień Wrzesień Październik Listopad Grudzień')
+PLCIE = models.IntegerChoices('PLEC', 'Kobieta Mężczyzna Inna')
+
+class Team(models.Model):
+    name = models.CharField(max_length=60)
+    country = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+    class Meta:
+        verbose_name = "Klub"
+        verbose_name_plural = "Kluby"
+
+class Osoba(models.Model):
+    PLEC_CHOICES = (
+        ("K", "Kobieta"),
+        ("M", "Mężczyzna"),
+        ("I", "Inna"),
+    )
+    
+    imie = models.CharField(max_length=40, blank = False, null = False)
+    nazwisko = models.CharField(max_length=60, blank = False, null = False)
+    plec = models.IntegerField(choices=PLCIE.choices, default=PLCIE.choices[2][0])
+    stanowisko = models.ForeignKey('Stanowisko', on_delete = models.CASCADE)
+    team = models.ForeignKey(Team, null=True, blank=True, on_delete=models.SET_NULL)
+    country = models.CharField(max_length=3, blank = False, default='', null = False)
+    data_dodania = models.DateField(default = date.today, blank=False, null=False)
+    
+    def __str__(self):
+        return f'{self.imie} {self.nazwisko}' 
+    
+    class Meta:
+        ordering = ["nazwisko"]
+        verbose_name = "Osoba"
+        verbose_name_plural = "Osoby"
+
+class Stanowisko(models.Model):
+    nazwa = models.CharField(max_length=80, blank = False, null = False)
+    opis = models.TextField(blank = False, null = False)
+    
+    def __str__(self):
+        return self.nazwa
+    
+    class Meta:
+        verbose_name = "Stanowisko"
+        verbose_name_plural = "Stanowiska"
