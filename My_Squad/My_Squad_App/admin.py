c0918e3467b23e79ca4dd635820ded96141d5f07
SzymonKw1 <skwiatkowski3@st.swps.edu.pl>
2025-01-17 13:36:50 +0100

    Zmiana widoków w admin panelu

--- My_Squad/My_Squad_App/admin.py
+++ My_Squad/My_Squad_App/admin.py
@@ -6,22 +6,43 @@ from .models import Osoba, Stanowisko, Zawodnik, Druzyna, Trener, Mecz, Statysty
 admin.site.register(Osoba)
 admin.site.register(Stanowisko)
 #usunąłem stąd zawodnika
-admin.site.register(Druzyna)
-admin.site.register(Trener)
-admin.site.register(Mecz)
-admin.site.register(StatystykiZawodnika)
-admin.site.register(Trening)
+#usunałem drużyna
+#usunalem trenera
+#usunalem mecz
+#usnąłem stąd statystykizawodnika
+#usunalem trening
 
 @admin.register(Zawodnik)
 class ZawodnikAdmin(admin.ModelAdmin):
-    list_display = ('nazwisko', 'pozycja')  
+    list_display = ('nazwisko', 'pozycja', 'numer_koszulki', 'druzyna' )  
     list_filter = ('pozycja',)  
     search_fields = ('nazwisko',)  
     ordering = ['pozycja', 'nazwisko']  
     
+@admin.register(StatystykiZawodnika)
+class StatystykiZawodnikaAdmin(admin.ModelAdmin):
+    # Wyświetlanie wszystkich pól w liście
+    list_display = ('zawodnik', 'mecz', 'bramki', 'asysty', 'zolte_kartki', 'czerwone_kartki', 'minuty_na_boisku')
+
+@admin.register(Druzyna)
+class DruzynaAdmin(admin.ModelAdmin):
+    list_display =('nazwa', 'miasto', 'stadion', 'trener')
+    list_filter = ('miasto',)
+    search_fields = ('nazwa',)
+
+@admin.register(Trener)
+class TrenerAdmin(admin.ModelAdmin):
+    list_display =('nazwisko', 'data_urodzenia', 'lata_doswiadczenia', 'narodowosc' )
+    ordering = ['lata_doswiadczenia']
 
 
+@admin.register(Mecz)
+class MeczAdmin(admin.ModelAdmin):
+    list_display = ('druzyna_gospodarz', 'druzyna_gosc', 'data', 'wynik_gospodarz', 'wynik_gosc' )
 
+@admin.register(Trening)
+class TreningAdmin(admin.ModelAdmin):
+    list_display = ('druzyna', 'data', 'czas_trwania_minuty','obszar_skupienia' )
 
 
 class OsobaAdmin(admin.ModelAdmin):
